9105a802473dba696b292598cfc51948ffab7f27
David Shestopal <dshestopal@yahoo.com>
2020-02-27 15:57:15 -0800

    poject

---
+++ onboarding/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Forms from './components/form';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        {' '}
+        <h1>ShmoneyGang</h1>
+      </header>
+
+      <Forms />
+    </div>
+  );
+}
+
+export default App;
